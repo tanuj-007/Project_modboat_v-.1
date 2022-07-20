c3f5e94cc086aad4431a1cd117ade53a7219c557
TanujSati <sattitanuj3@gmail.com>
2022-07-20 11:11:32 +0530

    Updates

---
+++ precache-manifest.c55c12f9e8d007b1bb6e506a3c79ec83.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0167c784411a7a2e69f2",
+    "url": "/Project_modboat_v-.1/static/css/main.7886c0a1.chunk.css"
+  },
+  {
+    "revision": "0167c784411a7a2e69f2",
+    "url": "/Project_modboat_v-.1/static/js/main.0167c784.chunk.js"
+  },
+  {
+    "revision": "d59e43034f3cbe1b4e93",
+    "url": "/Project_modboat_v-.1/static/js/1.d59e4303.chunk.js"
+  },
+  {
+    "revision": "79e229b6e307de0bb5a4",
+    "url": "/Project_modboat_v-.1/static/js/runtime~main.79e229b6.js"
+  },
+  {
+    "revision": "75c50b3609e27a5ef03d91136a392f1c",
+    "url": "/Project_modboat_v-.1/static/media/svg-1.75c50b36.svg"
+  },
+  {
+    "revision": "13812a65e6ba94d71ecdb5b051aea436",
+    "url": "/Project_modboat_v-.1/static/media/video.13812a65.mp4"
+  },
+  {
+    "revision": "e342f1a516fb4bc601dab4f620090768",
+    "url": "/Project_modboat_v-.1/static/media/svg-2.e342f1a5.svg"
+  },
+  {
+    "revision": "7fb89ecbe6f1dadb76834fd445d6eaaa",
+    "url": "/Project_modboat_v-.1/static/media/bg.7fb89ecb.png"
+  },
+  {
+    "revision": "2c7c56f4367fe35837fe9fcbf6243d57",
+    "url": "/Project_modboat_v-.1/static/media/d7.2c7c56f4.png"
+  },
+  {
+    "revision": "04b5fa201156bf376a90193252408c4b",
+    "url": "/Project_modboat_v-.1/static/media/d1.04b5fa20.png"
+  },
+  {
+    "revision": "0d552de83fa8334f5a81b35822fbfd0c",
+    "url": "/Project_modboat_v-.1/static/media/tim.0d552de8.png"
+  },
+  {
+    "revision": "d4c29ab72b59c6ffbddba082b821496e",
+    "url": "/Project_modboat_v-.1/static/media/logo.d4c29ab7.png"
+  },
+  {
+    "revision": "09e3aa34f6be0c9ad5174d2bab221030",
+    "url": "/Project_modboat_v-.1/index.html"
+  }
+];
